b59c46a4e86e3cf5acddb1d584f9ee4c5c72566b
adhilabu <adhilabacker@gmail.com>
2023-12-26 19:38:46 +0530

    feat: Updated .gitignore to exclude env folder

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from src.routers import analyse_router
+from src.routers import data_router
 
+app = FastAPI()
+import os
+
+workspace_path = os.getcwd()  # This gets the current working directory
+print("Workspace path:", workspace_path)
+# Include routers from different features
+# app.include_router(analyse_router, prefix="/analyse", tags=["analyse"])
+app.include_router(data_router)
+app.include_route
+# Add more routers for other features...
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
